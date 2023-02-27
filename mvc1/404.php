a396a748f28d2832e537b149d122b1e9865a0a84
ammy912 <110259042+ammy912@users.noreply.github.com>
2023-02-27 10:46:58 -0800

    Add files via upload

---
+++ mvc1/404.php
@@ -0,0 +1,26 @@
+<!-- banner -->
+<div class="inner_page-banner">
+</div>
+<!--//banner -->
+<!-- short -->
+ <div class="using-border py-3">
+   <div class="inner_breadcrumb  ml-4">
+      <ul class="short_ls">
+         <li>
+            <a href="index.html">Home</a>
+            <span>/ /</span>
+         </li>
+         <li>404 error</li>
+      </ul>
+   </div>
+</div>
+<!-- //short-->
+
+<!-- content start here -->
+<div class="container-fluid content">
+<div class="row">  
+<div class="col-md-12 p-3">
+<a href="<?php echo $mainurl;?>"><img src="<?php echo $baseurl;?>images/404.gif" class="img-fluid" style="width:80%; height:90%; margin-left:13%"></a>
+</div>
+</div>
+</div>
---
+++ mvc1/about.php
@@ -0,0 +1,227 @@
+<!-- banner -->
+<div class="inner_page-banner">
+</div>
+<!--//banner -->
+<!-- short -->
+<div class="using-border py-3">
+   <div class="inner_breadcrumb  ml-4">
+      <ul class="short_ls">
+         <li>
+            <a href="<?php echo $mainurl;?>">Home</a>
+            <span>/ /</span>
+         </li>
+         <li>About</li>
+      </ul>
+   </div>
+</div>
+<!-- //short-->
+<!--about-->
+<!--about-one-->
+<section class="about-inner py-lg-4 py-md-3 py-sm-3 py-3">
+   <div class="container py-lg-5 py-md-4 py-sm-4 py-3">
+      <h3 class="title text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">About Us</h3>
+      <div class="row abt-inner-agile">
+         <div class="col-lg-9 col-md-9 abt-inner-left">
+            <div class="row">
+               <div class="col-lg-8 col-md-8 p-0">
+                  <img src="<?php echo $baseurl;?>images/g1.jpg" class="img-thumbnail" alt="">
+               </div>
+               <div class="col-lg-4 col-md-4 p-0">
+                  <img src="<?php echo $baseurl;?>images/g7.jpg" class="img-thumbnail" alt="">
+                  <img src="<?php echo $baseurl;?>images/g1.jpg" class="img-thumbnail" alt="">
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-lg-4 col-md-4 p-0">
+                  <img src="<?php echo $baseurl;?>images/g2.jpg" class="img-thumbnail" alt="">
+               </div>
+               <div class="col-lg-8 col-md-8 text-center about-inner-mid">
+                  <h4 class="mb-lg-4 mb-md-3 mb-3">Treat yourself better</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-3 col-md-3 abt-inner-left p-0">
+            <img src="<?php echo $baseurl;?>images/2.jpg" class="img-thumbnail" alt="">
+         </div>
+      </div>
+   </div>
+</section>
+<!--//about-->
+<!--States-->
+<section class="about-inner-counter py-lg-4 py-md-3 py-sm-3 py-3">
+   <div class="container-fluid py-lg-5 py-md-4 py-sm-4 py-3">
+      <div class="stats-info row ">
+         <div class="col-lg-4 col-md-4 col-sm-4 stats-grid stats-grid-1">
+            <div class="register-left-agile">
+               <div class="counter">1200</div>
+               <div class="stat-info py-lg-3 py-md-3 py-sm-3 py-3">
+                  <h4>Coffee</h4>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-4 col-sm-4 stats-grid stats-grid-2">
+            <div class="register-left-agile">
+               <div class="counter">4000</div>
+               <div class="stat-info py-lg-3 py-md-3 py-sm-3 py-3">
+                  <h4>Designs</h4>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-4 col-sm-4 stats-grid stats-grid-3">
+            <div class="register-left-agile">
+               <div class="counter">500</div>
+               <div class="stat-info py-lg-3 py-md-3 py-sm-3 py-3">
+                  <h4>Clients</h4>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!--//States-->
+<!-- clients -->
+<section class="blog py-lg-4 py-md-3 py-sm-3 py-3">
+   <div class="container py-lg-5 py-md-5 py-sm-4 py-3">
+      <h3 class="title text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">Latest News</h3>
+      <div class="row blog-top-grids">
+         <div class="col-lg-6 left-side-agile top-client-gap">
+            <div class="least-w3layouts-text-gap">
+               <div class="row">
+                  <div class="col-md-4 col-sm-4 col-4 news-img">
+                     <img src="<?php echo $baseurl;?>images/t1.jpg" alt="" class="<?php echo $baseurl;?>image-fluid">
+                  </div>
+                  <div class="col-md-8 col-sm-8 col-8 news-agile-text ">
+                     <h4 class="mb-3">Milky Deo 
+                     </h4>
+                     <p >velit sagittis vehicula. Duis posuere 
+                        ex in mollis iaculis. Suspendisse tincidunt
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-6 left-side-agile ">
+            <div class="least-w3layouts-text-gap">
+               <div class="row ">
+                  <div class=" col-md-4 col-sm-4 col-4 news-img">
+                     <img src="<?php echo $baseurl;?>images/t2.jpg" alt="" class="<?php echo $baseurl;?>image-fluid">
+                  </div>
+                  <div class=" col-md-8 col-sm-8 col-8 news-agile-text ">
+                     <h4 class="mb-3">Rose Will
+                     </h4>
+                     <p >velit sagittis vehicula. Duis posuere 
+                        ex in mollis iaculis. Suspendisse tincidunt
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="row blog-top-grids">
+         <div class="col-lg-6 left-side-agile top-client-two">
+            <div class="least-w3layouts-text-gap">
+               <div class="row">
+                  <div class=" col-md-4 col-sm-4 col-4 news-img">
+                     <img src="<?php echo $baseurl;?>images/t3.jpg" alt="" class="<?php echo $baseurl;?>image-fluid">
+                  </div>
+                  <div class=" col-md-8 col-sm-8 col-8 news-agile-text ">
+                     <h4 class="mb-3">Sara Kent
+                     </h4>
+                     <p>velit sagittis vehicula. Duis posuere 
+                        ex in mollis iaculis. Suspendisse tincidunt
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-6 left-side-agile top-client-gap">
+            <div class="least-w3layouts-text-gap">
+               <div class="row">
+                  <div class=" col-md-4 col-sm-4 col-4 news-img">
+                     <img src="<?php echo $baseurl;?>images/t4.jpg" alt="" class="<?php echo $baseurl;?>image-fluid">
+                  </div>
+                  <div class=" col-md-8 col-sm-8 col-8 news-agile-text ">
+                     <h4 class="mb-3">Shilly Lana
+                     </h4>
+                     <p>velit sagittis vehicula. Duis posuere 
+                        ex in mollis iaculis. Suspendisse tincidunt 
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!--//clients -->
+<!-- Modal 1-->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+   <div class="modal-dialog" role="document">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Sign In</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+         <div class="modal-body">
+            <div class="register-form">
+               <form action="#" method="post">
+                  <div class="fields-grid">
+                     <div class="styled-input">
+                        <input type="text" placeholder="Your Name" name="Your Name" required="">
+                     </div>
+                     <div class="styled-input">
+                        <input type="email" placeholder="Your Email" name="Your Email" required="">
+                     </div>
+                     <div class="styled-input">
+                        <input type="password" placeholder="password" name="password" required="">
+                     </div>
+                     <button type="submit" class="btn-block mb-3">Sign In</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+         <div class="modal-footer">
+            <div class="sing-up-jeweler">
+               <a href="#" class="text-left" data-toggle="modal" data-target="#myModal3"> Don't Have an Account...?</a>
+            </div>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- //Modal 1-->
+<!-- Modal2 -->
+<div class="modal fade" id="myModal3" tabindex="-1" role="dialog">
+   <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+         <div class="modal-header_w3layouts_agile">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <div class="signin-form profile">
+               <div class="register-form">
+                  <h3 class="sign">Sign Up</h3>
+                  <div class="login-form">
+                     <form action="#" method="post">
+                        <input type="text" name="name" placeholder="Name" required="">
+                        <input type="email" name="email" placeholder="Email" required="">
+                        <input type="password" name="password" placeholder="Password" id="password" required="">
+                        <input type="password" placeholder="Confirm Password" id="confirm_password" required>
+                        <button type="submit" class="btn-block mb-3">Sign Up</button>
+                     </form>
+                  </div>
+               </div>
+               <div class="clearfix"></div>
+            </div>
+            <div class="modal-footer">
+               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+               <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- //Modal2 -->
